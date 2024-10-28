5c58f4fccb14cafa723410121d308e4c1893b50a
Felix Singgih <felixsinggih@gmail.com>
2024-10-28 10:55:33 +0700

    add: book ride function

---
+++ components/RideLayout.tsx
@@ -0,0 +1,58 @@
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { ReactNode, useRef } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { router } from "expo-router";
+import { icons } from "@/constants";
+import Map from "@/components/Map";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+
+const RideLayout = ({
+  title,
+  children,
+  snapPoints,
+}: {
+  title: string;
+  children: ReactNode;
+  snapPoints?: string[];
+}) => {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  return (
+    <GestureHandlerRootView>
+      <View className="flex-1 bg-white">
+        <View className="flex flex-col h-screen bg-blue-500">
+          <View className="flex flex-row absolute z-10 top-16 items-center justify-start px-5">
+            <TouchableOpacity onPress={() => router.back()}>
+              <View className="size-10 bg-white rounded-full items-center justify-center">
+                <Image
+                  source={icons.backArrow}
+                  resizeMode="contain"
+                  className="size-6"
+                />
+              </View>
+            </TouchableOpacity>
+
+            <Text className="text-xl font-JakartaSemiBold ml-5">
+              {title || "Go Back"}
+            </Text>
+          </View>
+
+          <Map />
+        </View>
+
+        <BottomSheet
+          keyboardBehavior="extend"
+          ref={bottomSheetRef}
+          snapPoints={snapPoints || ["40%", "85%"]}
+          index={0}
+        >
+          <BottomSheetView style={{ flex: 1, padding: 20 }}>
+            {children}
+          </BottomSheetView>
+        </BottomSheet>
+      </View>
+    </GestureHandlerRootView>
+  );
+};
+
+export default RideLayout;
